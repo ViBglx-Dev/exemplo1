8f873f8d6b4ad01199180c7566686c7609bc7a7d
ViBglx-Dev <vitoria.bougleux@aluno.senai.br>
2025-03-28 15:22:45 -0300

    exemplos4

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
   // Seleção pelo ID
-        let titulo = document.getElementById("titulo");
-        console.log(titulo);
+  let titulo = document.getElementById("titulo");
+  console.log(titulo);
         
-        // Seleção pela classe
-        let esportesPorClasse = document.getElementsByClassName("esporte");
-        console.log(esportesPorClasse);
+       // Seleção pela classe
+  let esportesPorClasse = document.getElementsByClassName("esporte");
+ console.log(esportesPorClasse);
         
-        // Seleção pela tag
-        let itensLista = document.getElementsByTagName("ul");
-        console.log(itensLista[0]);
+       // Seleção pela tag
+  let itensLista = document.getElementsByTagName("ul");
+   console.log(itensLista[0]);
         
-        let primeiroEsporte = document.querySelector(".esporte");
-        console.log(primeiroEsporte);
+  let primeiroEsporte = document.querySelector(".esporte");
+   console.log(primeiroEsporte);
         
-        let todosEsportes = document.querySelectorAll(".esporte");
-        console.log(todosEsportes);
+   let todosEsportes = document.querySelectorAll(".esporte");
+   console.log(todosEsportes);
+
+  titulo.innerText = "esportes";
+  titulo.innerHTML ="<span style='color:blue'>Meus esportes favoritos </span>"
+
+  let lista = document.getElementById("listadeesporte");
+
+  let novoesporte1 = document.createElement("li");
+  novoesporte1.innerText = "natação";
+
+  let novoesporte2 = document.createElement("li");
+  novoesporte2.innerText = "beach tenis";
+
+  listadeesporte.appendChild(novoesporte1);
+  listadeesporte.appendChild(novoesporte2);
